ee46855a5a234ac76a1cdaa26f397ea85482643e
yennhi24062002 <147739969+yennhi24062002@users.noreply.github.com>
2024-10-15 12:50:32 +0700

    Update hinhchunhat.cs

--- hinhchunhat.cs
+++ hinhchunhat.cs
@@ -52,3 +52,29 @@ namespace hinhchunhat
         }
     }
 }
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hinhchunhat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            hinhchunhat hcn= new hinhchunhat();
+            hcn.Nhap();
+            hcn.Xuat();
+            
+            float chuVi=hcn.TinhChuVi();
+            float dienTich=hcn.TinhDienTich();
+
+            Console.WriteLine("Chu vi la:" + chuVi);
+            Console.WriteLine("Dien tich la:" + dienTich);
+            Console.ReadLine();
+        }
+    }
+}
+
